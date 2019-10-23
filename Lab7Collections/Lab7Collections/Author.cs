8dc42472a15fdc79b657b47ee82ec734cbb9861b
Biniam <biniamt23@yahoo.com>
2019-10-23 10:50:04 -0700

    still working, dont want to mess up work so far

---
+++ Lab7Collections/Lab7Collections/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7Collections
+{
+    public class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Lab7Collections/Lab7Collections/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab7Collections
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+        public int NumberOfPages { get; set; }
+
+    }
+}
